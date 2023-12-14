5c823c781c86f27e328172500237c09badedc710
Grimkujow <130107205+Grimkujow@users.noreply.github.com>
2023-12-14 14:54:27 +0000

    debut creation shell + commentaires

--- scriptShell.sh
+++ scriptShell.sh
@@ -1,5 +1,9 @@
 #!/bash/bin
-mkdir -v temp
+#verifie + crée si non existant les fichiers temp et image
+mkdir -v temp 
 mkdir -v image
+
+
+#compile et lance main.c ( sans arguments, a modifier si possible)
 gcc -o exe progc/main.c
 ./exe

279dd7752ceebe392189973dad57d0ebc2bacc33
Grimkujow <130107205+Grimkujow@users.noreply.github.com>
2023-12-20 13:33:17 +0000

    case

--- scriptShell.sh
+++ scriptShell.sh
@@ -3,7 +3,23 @@
 mkdir -v temp 
 mkdir -v image
 
+echo "-------------------------------------------------"
+echo "$a"
 
+case $2 in 
+  *-d1*) echo "d1";;  
+  *-d2*) echo "d2";;  
+  *-l*) echo "l";;
+  *-t*) echo "t";;
+  *-s*) echo "s";;
+  *) echo "traitement non valable";; 
+  esac
+
+function aide {
+  echo "lancez aide.sh si vous avez besoin d'aide"
+}
+
+aide
 #compile et lance main.c ( sans arguments, a modifier si possible)
 gcc -o exe progc/main.c
 ./exe
